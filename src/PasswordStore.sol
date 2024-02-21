be77629eed6461ed483725fb73491373c57e06e6
Danail <vasilevd@vmware.com>
2024-02-21 09:03:07 +0200

    Added findings

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.18; // q: Is this the correct solidity version ?
 
 /*
  * @author not-so-secure-dev
@@ -11,6 +11,7 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    //@audit this is not private, everyone can read the blockchain
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +24,8 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+    // @audit everyone can set the password
+    // missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -30,6 +33,7 @@ contract PasswordStore {
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     // @audit there is no such a param
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
